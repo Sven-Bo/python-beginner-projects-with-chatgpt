c192630647021638a83f942d530b1e0926f443a3
Sven <67136317+Sven-Bo@users.noreply.github.com>
2023-01-28 08:51:46 +0100

    Initial Commit

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(page_title="Kickstart Your Next Project",
+                  page_icon=":rocket:",
+                  layout="centered")
+
+st.title("🚀 Python Beginner? Use ChatGPT's Power to Kickstart Your Next Project")
+
+st.markdown("Learn how to use the power of ChatGPT to kickstart your next Python project as a beginner. Discover how this language model can help you with code and idea generation.")
+
+st.markdown("""
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/nj60066.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Python Project Ideas For Beginners</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/qy65636.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Python Project Ideas For Beginners In Data Science</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/mu51246.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Virtual Plant Idea</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/jy61417.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Stock Dashboard Idea</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/ao38322.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Streamlit Portfolio Dashboard Idea</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/aa18460.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 How I created this website</a>
+""", unsafe_allow_html=True)
+
